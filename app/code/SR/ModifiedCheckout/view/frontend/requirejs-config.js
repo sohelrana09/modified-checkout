8a75cbef50ea290d5d826c1c8dfd139df29f281b
Sohel Rana <sohel.rana@ecommistry.com>
2017-12-17 20:48:24 +0600

    Modify overwrite issue

---
+++ app/code/SR/ModifiedCheckout/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/shipping': {
+                'SR_ModifiedCheckout/js/mixin/shipping-mixin': true
+            }
+        }
+    }
+};
--- app/code/SR/ModifiedCheckout/view/frontend/web/js/mixin/shipping-mixin.js
+++ app/code/SR/ModifiedCheckout/view/frontend/web/js/mixin/shipping-mixin.js
@@ -1,43 +1,43 @@
-define(
-    [
-        'jquery',
-        'underscore',
-        'Magento_Ui/js/form/form',
-        'ko',
-        'Magento_Customer/js/model/customer',
-        'Magento_Customer/js/model/address-list',
-        'Magento_Checkout/js/model/address-converter',
-        'Magento_Checkout/js/model/quote',
-        'Magento_Checkout/js/action/create-shipping-address',
-        'Magento_Checkout/js/action/select-shipping-address',
-        'Magento_Checkout/js/view/shipping',
-        'Magento_Checkout/js/action/create-billing-address',
-        'Magento_Checkout/js/action/select-billing-address',
-        'Magento_Checkout/js/action/set-shipping-information',
-        'Magento_Checkout/js/model/step-navigator',
-        'Magento_Checkout/js/checkout-data'
-    ],
-    function(
-        $,
-        _,
-        Component,
-        ko,
-        customer,
-        addressList,
-        addressConverter,
-        quote,
-        createShippingAddress,
-        selectShippingAddress,
-        shipping,
-        createBillingAddress,
-        selectBillingAddress,
-        setShippingInformationAction,
-        stepNavigator,
-        checkoutData
-    ) {
-        'use strict';
+define([
+    'jquery',
+    'underscore',
+    'Magento_Ui/js/form/form',
+    'ko',
+    'Magento_Customer/js/model/customer',
+    'Magento_Customer/js/model/address-list',
+    'Magento_Checkout/js/model/address-converter',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/action/create-shipping-address',
+    'Magento_Checkout/js/action/select-shipping-address',
+    'Magento_Checkout/js/action/create-billing-address',
+    'Magento_Checkout/js/action/select-billing-address',
+    'Magento_Checkout/js/action/set-shipping-information',
+    'Magento_Checkout/js/model/step-navigator',
+    'Magento_Checkout/js/checkout-data'
+], function(
+    $,
+    _,
+    Component,
+    ko,
+    customer,
+    addressList,
+    addressConverter,
+    quote,
+    createShippingAddress,
+    selectShippingAddress,
+    createBillingAddress,
+    selectBillingAddress,
+    setShippingInformationAction,
+    stepNavigator,
+    checkoutData
+) {
+    'use strict';
 
-        _.extend(shipping.prototype, {
+    return function (target) {
+        return target.extend({
+            defaults: {
+                template: 'SR_ModifiedCheckout/address'
+            },
             setShippingInformation: function () {
                 if (this.validateShippingInformation() && this.validateBillingInformation()) {
                     setShippingInformationAction().done(
@@ -109,14 +109,12 @@ define(
                 if ($('#billing-save-in-address-book').is(":checked")) {
                     addressData.save_in_address_book = 1;
                 }
-                
+
                 newBillingAddress = createBillingAddress(addressData);
                 selectBillingAddress(newBillingAddress);
 
                 return true;
             }
         });
-
-        return Component.extend({});
     }
-);
+});
